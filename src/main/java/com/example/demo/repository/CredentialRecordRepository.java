2e850bab1da75774306553efcd286626daa8d69b
bala-amypo <bala@amypo.in>
2025-12-20 11:09:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialRecordRepository.java
+++ src/main/java/com/example/demo/repository/CredentialRecordRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.CredentialRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,13 +13,12 @@ public interface CredentialRecordRepository
         extends JpaRepository<CredentialRecord, Long> {
 
    
-    List<CredentialRecord> findExpiredBefore(LocalDate date);
+    @Query("SELECT c FROM CredentialRecord c WHERE c.expiryDate < :date")
+    List<CredentialRecord> findExpiredBefore(@Param("date") LocalDate date);
 
-    
     @Query("SELECT c FROM CredentialRecord c WHERE c.status = ?1")
     List<CredentialRecord> findByStatusUsingHql(String status);
 
-  
     @Query("SELECT c FROM CredentialRecord c WHERE c.issuer = ?1 AND c.credentialType = ?2")
     List<CredentialRecord> searchByIssuerAndType(String issuer, String type);
 
