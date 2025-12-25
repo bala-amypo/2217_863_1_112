ac8f4506654dd363a493fc3845f6be2affdc8528
bala-amypo <bala@amypo.in>
2025-12-25 14:46:50 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,18 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
 import com.example.demo.servlet.SimpleStatusServlet;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
+    
     @Bean
     public ServletRegistrationBean<SimpleStatusServlet> statusServlet() {
-        return new ServletRegistrationBean<>(new SimpleStatusServlet(), "/status");
+        ServletRegistrationBean<SimpleStatusServlet> bean = 
+            new ServletRegistrationBean<>(new SimpleStatusServlet(), "/status");
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 }
